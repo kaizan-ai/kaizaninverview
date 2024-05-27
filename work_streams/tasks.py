48244592ce2e5c30cc3c2bac996f11e8ef5c8a61
Spiridon Alexandru <spiridon.g.alex@gmail.com>
2024-05-27 11:43:33 +0300

    Fixed typo

--- work_streams/tasks.py
+++ work_streams/tasks.py
@@ -6,6 +6,6 @@ from work_streams.models import WorkStream
 @app.task
 def do_something_with_work_streams():
     for work_stream in WorkStream.objects.all():
-        entry_count = WorkInteraction.objects.filter(work_stream=work_stream).count()
-        work_stream.title = f"{work_stream.title} ({entry_count})"
+        work_interaction_count = WorkInteraction.objects.filter(work_stream=work_stream).count()
+        work_stream.title = f"{work_stream.title} ({work_interaction_count})"
         work_stream.save()
